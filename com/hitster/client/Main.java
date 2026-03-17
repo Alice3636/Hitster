f0ff7b2994a28f5965e91bdeb0c54ec5290480ea
Alice3636 <alice.ozz.36@gmail.com>
2026-03-17 14:41:07 +0200

    committed

---
+++ com/hitster/client/Main.java
@@ -0,0 +1,25 @@
+package com.hitster.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../resources/views/lobby.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setTitle("Hitster Online");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false); 
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); 
+    }
+}
---
+++ com/hitster/client/controllers/LeaderboardController.java
@@ -0,0 +1,5 @@
+package com.hitster.client.controllers;
+
+public class LeaderboardController {
+     
+}
