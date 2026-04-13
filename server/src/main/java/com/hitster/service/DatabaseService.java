b95283bc83510de484ae54b97e61354ae8abcc8f
Maayan12 <99267673+Maayan12@users.noreply.github.com>
2026-04-13 17:42:17 +0300

    delete GameStateMapper cause it is not updated (need to be fixed)

--- server/src/main/java/com/hitster/service/DatabaseService.java
+++ server/src/main/java/com/hitster/service/DatabaseService.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
-import com.hitster.DBManager;
+import com.hitster.repository.DBManager;
 
 public class DatabaseService {
 
